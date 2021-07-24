be22b5b81d9784901ef0d572b7366dd74f15fcd4
Zack <ahmedsakaria32@gmail.com>
2021-07-24 13:47:16 -0500

    setting up the roles table, made the id of employee Auto-Incremnt

--- Assets/seeds.sql
+++ Assets/seeds.sql
@@ -1,6 +1,17 @@
+//inserting Departments
 INSERT INTO department (department)
 VALUE ('Accounting');
 INSERT INTO department (department)
-VALUE ('Manegment');
+VALUE ('Management');
 INSERT INTO department (department)
 VALUE ('Service');
+INSERT INTO department (department)
+VALUE ('Sales');
+
+//inserting Roles
+INSERT INTO role (title, salary, department_id)
+VALUE ('Accountant', 70000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Supervisor', 60000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Sales', 60000, 4);
