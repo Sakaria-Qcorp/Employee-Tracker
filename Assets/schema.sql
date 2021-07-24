8fdc05f0e21730eee67830421aa94a705203c3fe
Zack <ahmedsakaria32@gmail.com>
2021-07-24 11:02:51 -0500

    added the the department to the schema

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE department (
+	department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY(department_id)
+);
